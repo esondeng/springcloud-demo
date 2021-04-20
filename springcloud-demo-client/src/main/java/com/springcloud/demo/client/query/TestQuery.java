22b69c1eb59deaf6555ea418bba1af2b5019f02e
dengxiaolin <dengxiaolin@meituan.com>
2021-04-20 16:08:16 +0800

    spring cloud demo

---
+++ springcloud-demo-client/src/main/java/com/springcloud/demo/client/query/TestQuery.java
@@ -0,0 +1,10 @@
+package com.springcloud.demo.client.query;
+
+/**
+ * @author dengxiaolin
+ * @since 2021/04/20
+ */
+public class TestQuery {
+    private String name;
+    private Integer age;
+}
---
+++ springcloud-demo-client/src/main/java/com/springcloud/demo/client/response/TestTo.java
@@ -0,0 +1,15 @@
+package com.springcloud.demo.client.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author dengxiaolin
+ * @since 2021/04/20
+ */
+@Getter
+@Setter
+public class TestTo {
+    private String name;
+    private String score;
+}
